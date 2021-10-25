a9e4e2b0d5a393dc58917e7796f705633bec7f5e
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-10-25 15:54:10 +0200

    Add files via upload

---
+++ School/script.js
@@ -0,0 +1,15 @@
+// // zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+
+// ikRockArrowFunctions()
+
+let a = 10;
+
+const fivePlusSeven = myFunction => a + 8
+
+fivePlusSeven()
+
+const createObject = () => ({greeting: "hoi"})
+
+createObject()
+
